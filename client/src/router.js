7020cb834f821035d372148bf5eed567b636f3ad
Jesray Garciano <jesray.garciano@gmail.com>
2018-12-05 11:02:57 +0800

    GraphQL constans query

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Signup from './views/Signup.vue'
 
 Vue.use(Router)
 
@@ -10,7 +11,20 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
-    }
+      component: Home,
+      meta: { title: 'Bugha'}
+    },
+    {
+      path: '/signup/:id',
+      name: 'signup',
+      component: Signup,
+      meta: { title: 'Signup - Bugha'}
+    },
   ]
 })
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
+})
+
+export default router
